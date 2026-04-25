c11859064d3c1a91425fef318defde5ade374d2c
LDGnx-dev <166982620+LDGnx-dev@users.noreply.github.com>
2026-04-25 04:23:13 -0700

    Mayor changes and modularity implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
-// https://astro.build/config
+import vercel from '@astrojs/vercel/serverless'; // Asegúrate de que termine en /serverless
+
 export default defineConfig({
+  // Esto es crucial para Vercel
+  output: 'server', 
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [react()],
-  adapter: vercel()
 });
